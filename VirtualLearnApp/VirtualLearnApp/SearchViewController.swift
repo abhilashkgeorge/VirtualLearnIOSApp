2b3dd4d4cf2b072da463244438ca379799456b92
Abhilash Knox George <user.abhilash@gmail.com>
2022-04-07 12:09:06 +0530

    added search screen

---
+++ VirtualLearnApp/VirtualLearnApp/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  VirtualLearnApp
+//
+//  Created by Abhilash k George on 07/04/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
