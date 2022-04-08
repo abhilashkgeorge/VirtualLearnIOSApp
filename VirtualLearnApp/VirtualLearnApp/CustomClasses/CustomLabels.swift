c3c11b07edb59fb6fe83427c3d618a8299653a84
Abhilash Knox George <user.abhilash@gmail.com>
2022-04-08 11:38:09 +0530

    added custom classes

---
+++ VirtualLearnApp/VirtualLearnApp/CustomClasses/CustomLabels.swift
@@ -0,0 +1,20 @@
+//
+//  CustomLabels.swift
+//  VirtualLearnApp
+//
+//  Created by Abhilash k George on 08/04/22.
+//
+
+import UIKit
+
+class CustomLabels: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ VirtualLearnApp/VirtualLearnApp/CustomClasses/CustomTextFeilds.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTextFeild.swift
+//  VirtualLearnApp
+//
+//  Created by Abhilash k George on 08/04/22.
+//
+
+import UIKit
+
+class helloworld: UITextField {
+
+}
+
+class second: UITextField {
+    
+}
