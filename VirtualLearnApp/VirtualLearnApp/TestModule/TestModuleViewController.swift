2148586994a4231945732a104f9849b05022e0ba
Prajnap <prajna.p@robosoftin.com>
2022-04-19 17:18:57 +0530

    Added Test Module

---
+++ VirtualLearnApp/VirtualLearnApp/TestModule/TestModuleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestModuleViewController.swift
+//  VirtualLearnApp
+//
+//  Created by Prajna P on 19/04/22.
+//
+
+import UIKit
+
+class TestModuleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
