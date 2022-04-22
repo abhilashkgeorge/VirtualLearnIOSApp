4d6a5da0f7e7a4cd34980efc88cc1aadb2c4e1eb
Prajnap <prajna.p@robosoftin.com>
2022-04-22 11:27:30 +0530

    added test  updated ui and notifications api

---
+++ VirtualLearnApp/VirtualLearnApp/Notifications/NotificationModel.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationModel.swift
+//  VirtualLearnApp
+//
+//  Created by Prajna P on 22/04/22.
+//
+
+import Foundation
--- VirtualLearnApp/VirtualLearnApp/TestModule/TestModuleViewController.swift
+++ VirtualLearnApp/VirtualLearnApp/TestModule/TestModuleViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class TestModuleViewController: UIViewController {
-
+    @IBOutlet weak var option1: UIView!
+    @IBOutlet weak var option2: UIView!
+    @IBOutlet weak var option3: UIView!
+    @IBOutlet weak var option4: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        optionsUI()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func optionsUI() {
+        option1.layer.borderWidth = 0.5
+        option2.layer.borderWidth = 0.5
+        option3.layer.borderWidth = 0.5
+        option4.layer.borderWidth = 0.5
+        
+        option1.layer.borderColor = UIColor.clear.cgColor
+        option2.layer.borderColor = UIColor.clear.cgColor
+        option3.layer.borderColor = UIColor.clear.cgColor
+        option4.layer.borderColor = UIColor.clear.cgColor
+        
+        option1.layer.shadowColor = UIColor.lightGray.cgColor
+        option1.layer.shadowOpacity = 0.3
+        option1.layer.shadowRadius = 2
+        option1.layer.shadowOffset = .zero
+        option2.layer.shadowColor = UIColor.lightGray.cgColor
+        option2.layer.shadowOpacity = 0.3
+        option2.layer.shadowRadius = 2
+        option2.layer.shadowOffset = .zero
+        option3.layer.shadowColor = UIColor.lightGray.cgColor
+        option3.layer.shadowOpacity = 0.3
+        option3.layer.shadowRadius = 2
+        option3.layer.shadowOffset = .zero
+        option4.layer.shadowColor = UIColor.lightGray.cgColor
+        option4.layer.shadowOpacity = 0.3
+        option4.layer.shadowRadius = 2
+        option4.layer.shadowOffset = .zero
+        
+        option1.layer.cornerRadius = 5
+        option2.layer.cornerRadius = 5
+        option3.layer.cornerRadius = 5
+        option4.layer.cornerRadius = 5
+        
+        
     }
-    */
+    
 
 }
