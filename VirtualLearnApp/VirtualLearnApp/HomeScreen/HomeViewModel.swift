5b5467088f7bd9b1e4d174a06eb773810da79c54
Anushree <anushree@robosoftin.com>
2022-04-24 17:32:37 +0530

    fixed issues with home api

--- VirtualLearnApp/VirtualLearnApp/HomeScreen/HomeViewModel.swift
+++ VirtualLearnApp/VirtualLearnApp/HomeScreen/HomeViewModel.swift
@@ -16,9 +16,9 @@ class HomeViewModel {
     var chapters = 0
     var imageString = ""
     var courseImage = UIImage()
+    
     var allUserCourses = [HomeModel]()
     var userPopularCourses = [HomeModel]()
-    
     let manager = HomeNetworkManager()
     let imageApi = ApiImage()
     
@@ -74,24 +74,84 @@ class HomeViewModel {
     func serializePopularCourses(json: Any)-> [HomeModel] {
         
         let jsonData = json as? [String: Any]
-        let popularCourses = jsonData!["popular"] as? [[String: Any]] ?? [["Popular Courses": "error"]]
+        let popularCourses = jsonData!["popular"] as? [String: Any] ?? ["Popular Courses": "error"]
         
-        for course in popularCourses {
+        let designCourses = popularCourses["Design"] as? [[String: Any]] ?? [["Design courses": "error"]]
+        
+        for eachCourse in designCourses {
             
-            id = course["_id"] as? String ?? "No all course ID"
-            title = course["courseName"] as? String ?? "No popular course name"
-            category = course["category"] as? String ?? "No popular course category"
-            chapters = course["totalChapters"] as? Int ?? 0
-            imageString = course["courseImg"] as? String ?? "No popular course image"
+            id = eachCourse["_id"] as? String ?? "No design course ID"
+            title = eachCourse["courseName"] as? String ?? "No design course name"
+            category = eachCourse["category"] as? String ?? "No design course category"
+            chapters = eachCourse["totalChapters"] as? Int ?? 0
+            imageString = eachCourse["courseImg"] as? String ?? "No design course image"
             
             imageApi.getImgFromApi(url: imageString) { (image) in
                 self.courseImage = image
             }
             
             let eachPopularCourse = HomeModel(id: id, courseTitle: title, courseCategory: category, noOfChapters: chapters, courseImage: courseImage)
+            print(eachPopularCourse.courseTitle)
             userPopularCourses.append(eachPopularCourse)
         }
-        print(userPopularCourses[0].courseTitle)
+        
+        let developmentCourses = popularCourses["Development"] as? [[String: Any]] ?? [["Development courses": "error"]]
+        
+        for eachCourse in developmentCourses {
+            
+            id = eachCourse["_id"] as? String ?? "No development course ID"
+            title = eachCourse["courseName"] as? String ?? "No development course name"
+            category = eachCourse["category"] as? String ?? "No development course category"
+            chapters = eachCourse["totalChapters"] as? Int ?? 0
+            imageString = eachCourse["courseImg"] as? String ?? "No development course image"
+            
+            imageApi.getImgFromApi(url: imageString) { (image) in
+                self.courseImage = image
+            }
+            
+            let eachPopularCourse = HomeModel(id: id, courseTitle: title, courseCategory: category, noOfChapters: chapters, courseImage: courseImage)
+            print(eachPopularCourse.courseTitle)
+            userPopularCourses.append(eachPopularCourse)
+        }
+        
+        let photographyCourses = popularCourses["Photography"] as? [[String: Any]] ?? [["Photography courses": "error"]]
+        
+        for eachCourse in photographyCourses {
+            
+            id = eachCourse["_id"] as? String ?? "No photography course ID"
+            title = eachCourse["courseName"] as? String ?? "No photography course name"
+            category = eachCourse["category"] as? String ?? "No photography course category"
+            chapters = eachCourse["totalChapters"] as? Int ?? 0
+            imageString = eachCourse["courseImg"] as? String ?? "No photography course image"
+            
+            imageApi.getImgFromApi(url: imageString) { (image) in
+                self.courseImage = image
+            }
+            
+            let eachPopularCourse = HomeModel(id: id, courseTitle: title, courseCategory: category, noOfChapters: chapters, courseImage: courseImage)
+            print(eachPopularCourse.courseTitle)
+            userPopularCourses.append(eachPopularCourse)
+        }
+        
+        let lifeStyleCourses = popularCourses["Lifestyle"] as? [[String: Any]] ?? [["LifeStyle courses": "error"]]
+        
+        for eachCourse in lifeStyleCourses {
+            
+            id = eachCourse["_id"] as? String ?? "No lifeStyle course ID"
+            title = eachCourse["courseName"] as? String ?? "No lifeStyle course name"
+            category = eachCourse["category"] as? String ?? "No lifeStyle course category"
+            chapters = eachCourse["totalChapters"] as? Int ?? 0
+            imageString = eachCourse["courseImg"] as? String ?? "No lifeStyle course image"
+            
+            imageApi.getImgFromApi(url: imageString) { (image) in
+                self.courseImage = image
+            }
+            
+            let eachPopularCourse = HomeModel(id: id, courseTitle: title, courseCategory: category, noOfChapters: chapters, courseImage: courseImage)
+            print(eachPopularCourse.courseTitle)
+            userPopularCourses.append(eachPopularCourse)
+        }
+        
         return userPopularCourses
     }
     
