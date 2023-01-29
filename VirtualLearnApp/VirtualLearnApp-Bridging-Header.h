aa7b29d3d90d1ebede1b99ba0b3da04208050031
Abhilash Knox George <user.abhilash@gmail.com>
2023-01-29 20:49:51 +0530

    Added Pod file

---
+++ VirtualLearnApp/VirtualLearnApp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  VirtualLearnApp-Bridging-Header.h
+//  
+//
+//  Created by Abhilash k George on 29/01/23.
+//
+
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Branch/Branch.h>
+#import <Branch/BranchUniversalObject.h>
+#import <Branch/BranchLinkProperties.h>
